48ca20b33db8d56eb1d262ee2a5d4b84e3a0752b
Taichi Ogawa <ta1ogw0970@outlook.jp>
2024-12-01 23:48:51 +0900

    test

--- hackason/main.go
+++ hackason/main.go
@@ -1,74 +1,36 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"log"
+	"encoding/json"
 	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"uttc_hackason_be/controller"
-	dao2 "uttc_hackason_be/dao"
-
-	_ "github.com/go-sql-driver/mysql"
 )
 
-var db *sql.DB
-var dao *dao2.UserDao
-
-func init() {
-	mysqlUser := os.Getenv("MYSQL_USER")
-	mysqlPwd := os.Getenv("MYSQL_PWD")
-	mysqlHost := os.Getenv("MYSQL_HOST")
-	mysqlDatabase := os.Getenv("MYSQL_DATABASE")
-
-	connStr := fmt.Sprintf("%s:%s@%s/%s", mysqlUser, mysqlPwd, mysqlHost, mysqlDatabase)
-	db, err := sql.Open("mysql", connStr)
-	if err != nil {
-		log.Fatalf("fail: sql.Open, %v\n", err)
-	}
-	if err := db.Ping(); err != nil {
-		log.Fatalf("fail: _db.Ping, %v\n", err)
-	}
-	dao = dao2.CreateDao(db)
+type responseMessage struct {
+	Message string `json:"message"`
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		controller.GetUserController(w, r, dao)
-	case http.MethodPost:
-		controller.RegisterUserController(w, r, dao)
-	default:
-		log.Printf("fail: HTTP Method is %s\n", r.Method)
+	name := r.URL.Query().Get("name")
+	if r.Method != http.MethodGet {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
+	if name == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	bytes, err := json.Marshal(responseMessage{
+		Message: "Hello " + name + "-san.",
+	})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(bytes)
 }
 
 func main() {
-	http.HandleFunc("/user", handler)
-	closeDBWithSysCall()
-
-	log.Println("Listening...")
-	port := os.Getenv("PORT")
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func closeDBWithSysCall() {
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, syscall.SIGTERM, syscall.SIGINT)
-	go func() {
-		s := <-sig
-		log.Printf("received syscall, %v", s)
-
-		if err := db.Close(); err != nil {
-			log.Fatal(err)
-		}
-		log.Printf("success: db.Close()")
-		os.Exit(0)
-	}()
+	http.HandleFunc("/hello", handler)
+	http.ListenAndServe(":8000", nil)
 }
