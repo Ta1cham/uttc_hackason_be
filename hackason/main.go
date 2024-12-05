6276db3caef10a71cd6a73845b245401bfbaf48c
Taichi Ogawa <ta1ogw0970@outlook.jp>
2024-12-06 00:14:39 +0900

    main

--- hackason/main.go
+++ hackason/main.go
@@ -2,31 +2,17 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
 	"log"
-	"math/rand"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
-	"unicode/utf8"
+	"uttc_hackason_be/controller"
+	"uttc_hackason_be/dao"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/oklog/ulid/v2"
 )
-
-type UserResForHTTPGET struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
-	Age  int    `json:"age"`
-}
-
-type postData struct {
-	Name string `json:"name"`
-	Age  int    `json:"age"`
-}
 
 var db *sql.DB
 
@@ -37,118 +23,42 @@ func init() {
 	mysqlDatabase := os.Getenv("MYSQL_DATABASE")
 
 	connStr := fmt.Sprintf("%s:%s@%s/%s", mysqlUser, mysqlPwd, mysqlHost, mysqlDatabase)
-	_db, err := sql.Open("mysql", connStr)
+	var err error
+	db, err = sql.Open("mysql", connStr)
 	if err != nil {
 		log.Fatalf("fail: sql.Open, %v\n", err)
 	}
-	if err := _db.Ping(); err != nil {
+	if err := db.Ping(); err != nil {
 		log.Fatalf("fail: _db.Ping, %v\n", err)
 	}
-	db = _db
 }
 
-func isInvalid(data postData) bool {
-	if data.Name == "" || utf8.RuneCountInString(data.Name) > 50 || data.Age < 20 || data.Age > 80 {
-		return true
-	}
-	return false
-}
+//func init() {
+//	mysqlUser := "user"
+//	mysqlUserpsw := "password"
+//	mysqlDatabase := "mydatabase"
+//
+//	_db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@(localhost:3306)/%s", mysqlUser, mysqlUserpsw, mysqlDatabase))
+//	if err != nil {
+//		log.Fatalf("fail: sql.Open, %v\n", err)
+//	}
+//	if err := _db.Ping(); err != nil {
+//		log.Fatalf("fail: _db.Ping, %v\n", err)
+//	}
+//	db = _db
+//}
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	userdao := dao.CreateDao(db)
 	switch r.Method {
 	case http.MethodGet:
-		name := r.URL.Query().Get("name")
-		if name == "" {
-			log.Printf("fail: name is empty")
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		rows, err := db.Query("SELECT id, name, age FROM user WHERE name = ?", name)
-		if err != nil {
-			log.Printf("fail: db.Query, %v\n", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		users := make([]UserResForHTTPGET, 0)
-		for rows.Next() {
-			var u UserResForHTTPGET
-			if err := rows.Scan(&u.ID, &u.Name, &u.Age); err != nil {
-				log.Printf("fail: rows.Scan, %v\n", err)
-
-				if err := rows.Close(); err != nil {
-					log.Printf("fail: rows.Close(), %v\n", err)
-				}
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			users = append(users, u)
-		}
-
-		bytes, err := json.Marshal(users)
-		if err != nil {
-			log.Printf("fail: json.Marshal, %v\n", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		w.Header().Set("Content-type", "application/json")
-		w.Write(bytes)
+		controller.GetUserController(w, r, userdao)
 	case http.MethodPost:
-		var data postData
-		if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
-			log.Printf("fail: json.NewDecoder.Decode, %v\n", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		if isInvalid(data) {
-			log.Printf("fail: POST data is invalid")
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		// ulidの生成
-		t := time.Now()
-		entropy := ulid.Monotonic(rand.New(rand.NewSource(t.UnixNano())), 0)
-		id := ulid.MustNew(ulid.Timestamp(t), entropy)
-
-		// トランザクションの開始
-		tx, err := db.Begin()
-		if err != nil {
-			log.Printf("fail: db.Begin, %v\n", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		if _, err := tx.Exec("INSERT INTO user (id, name, age) VALUES (?, ?, ?)", id.String(), data.Name, data.Age); err != nil {
-			tx.Rollback()
-			log.Printf("fail: INSERT, %v\n", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		if err := tx.Commit(); err != nil {
-			log.Printf("fail: tx.Commit, %v\n", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		// 結果の出力
-		w.WriteHeader(http.StatusOK)
-		content := map[string]string{
-			"id": id.String(),
-		}
-		bytes, err := json.Marshal(content)
-		if err != nil {
-			log.Printf("fail: json.Marshal, %v\n", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		w.Header().Set("Content-type", "application/json")
-		w.Write(bytes)
+		controller.RegisterUserController(w, r, userdao)
 	default:
 		log.Printf("fail: HTTP Method is %s\n", r.Method)
 		w.WriteHeader(http.StatusBadRequest)
 		return
-
 	}
 }
 
